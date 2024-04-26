7835d1556c9f2736ab67f324dd956312076a5798
EviannyRM <167649603+EviannyRM@users.noreply.github.com>
2024-04-26 18:41:35 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Práctica_1
+{
+    internal class Practica1
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Ingrese el primer numero: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo numero: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int suma = num1 + num2;
+
+            int multiplicacion = num1 * num2;
+
+            Console.WriteLine($"El resultado de la suma es: {suma}");
+
+            Console.WriteLine($"El resultado de la multiplicacion es: {multiplicacion}");
+        }
+    }
+
+
+}
